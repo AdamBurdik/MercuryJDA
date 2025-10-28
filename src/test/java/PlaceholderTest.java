15595a6836f59ac613f6eafa938b92b16040cae8
AdamIx <adamburdik9@gmail.com>
2025-10-28 14:24:19 +0100

    Add: more tests for placeholders

--- src/test/java/PlaceholderTest.java
+++ src/test/java/PlaceholderTest.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class PlaceholderTest {
-	@Test
-	public void testPlaceholderManagerParsing() throws ParsingException {
-		PlaceholderManager placeholderManager = new PlaceholderManager();
+	private final PlaceholderManager placeholderManager = new PlaceholderManager();
 
+	@Test
+	public void testDataPlaceholder() throws ParsingException {
 		record Data(
 				@NotNull String name,
 				int age
@@ -33,5 +33,69 @@ public class PlaceholderTest {
 		String text = "Hello, <test:name>! You are <test:age>yo.";
 		String parsed = placeholderManager.parse(text, List.of(data));
 		assertEquals(parsed, "Hello, " + data.name + "! You are " + data.age + "yo.");
+	}
+
+	@Test
+	public void testGlobalPlaceholder() throws ParsingException {
+		placeholderManager.registerGlobalPlaceholder("ping", args -> "Pong!");
+
+		String text = "Hello, <ping>";
+		String parsed = placeholderManager.parse(text);
+		assertEquals("Hello, " + "Pong!", parsed);
+	}
+
+	@Test
+	public void testRedirectPlaceholder() throws ParsingException {
+		record Data(
+				@NotNull String name,
+				int age
+		) {}
+
+		record Container(
+				int id,
+				@NotNull Data data
+		) {}
+
+		Data data = new Data("John Doe", 25);
+		Container container = new Container(10, data);
+
+		placeholderManager.registerPlaceholder("test", Data.class, (args, d) -> {
+			if (!args.hasNext()) return d.toString();
+
+			return String.valueOf(switch (args.pop()) {
+				case "name" -> d.name;
+				case "age" -> d.age;
+				default -> throw new IllegalStateException("Unexpected value: " + args.pop());
+			});
+		});
+
+		placeholderManager.registerRedirect(Container.class, cont -> List.of(cont.data));
+
+		String text = "Hello, <test:name>! You are <test:age>yo.";
+		String parsed = placeholderManager.parse(text, List.of(container));
+		assertEquals(parsed, "Hello, " + data.name + "! You are " + data.age + "yo.");
+	}
+
+	@Test
+	public void testPlaceholderEscaping() throws ParsingException {
+		record Data(
+				@NotNull String name,
+				int age
+		) {}
+
+		placeholderManager.registerPlaceholder("test", Data.class, (args, data) -> {
+			if (!args.hasNext()) return data.toString();
+
+			return String.valueOf(switch (args.pop()) {
+				case "name" -> data.name;
+				case "age" -> data.age;
+				default -> throw new IllegalStateException("Unexpected value: " + args.pop());
+			});
+		});
+
+		Data data = new Data("john doe", 20);
+		String text = "Hello, <\\test:name>! You are <\\test:age>yo.";
+		String parsed = placeholderManager.parse(text, List.of(data));
+		assertEquals("Hello, <test:name>! You are <test:age>yo.", parsed);
 	}
 }
